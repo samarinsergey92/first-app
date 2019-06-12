12e6c47f1a682764682d5376c70dee2968265a54
Sergey Samarin <sergei.sam.92@gmail.com>
2019-06-12 17:03:28 +0300

    This is my first commit

---
+++ app/src/main/java/com/samarinsergey/firsttestapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.samarinsergey.firsttestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editInput = findViewById(R.id.edit_input);
+        Button buttonEnter = findViewById(R.id.button_enter);
+        final TextView textResult = findViewById(R.id.text_result);
+
+        buttonEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textResult.setText(editInput.getText().toString());
+            }
+        });
+    }
+}
